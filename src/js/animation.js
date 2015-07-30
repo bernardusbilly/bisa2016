e15f8f8931805a89d9a11eb83a973045668020db
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2015-07-30 09:05:55 -0700

    Added section 3

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,8 +1,18 @@
 $(window).ready(function() {
-	$('.hidden-title').hide();
-	$('.title').hover(function() {
-		$('.hidden-title').fadeIn();
+	$('.info-officer-group').hide();
+	$('.info-officer-photo').hover(function() {
+		var name = $(this).children().attr('data-trigger');
+		console.log(name + " is hovered.");
+		$('.'+name).show();
+		$('.'+name).removeClass("animated fadeOut");
+		$('.'+name).addClass("animated fadeIn");
+		$(this).removeClass("photo-bw");
 	}, function() {
-		$('.hidden-title').fadeOut();
+		// leave hover
+		var name = $(this).children().attr('data-trigger');
+		console.log(name + " is hovered.");
+		$('.'+name).removeClass("animated fadeIn");
+		$('.'+name).addClass("animated fadeOut");
+		$(this).addClass("photo-bw");
 	});
 });
