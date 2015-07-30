c708b2f38af12658aa126d00aa21d2286d703c7e
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2015-07-30 16:25:47 -0700

    Fix bug: first appearance on section 1

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,18 +1,28 @@
 $(window).ready(function() {
+	$('.home-title').hide(); // Fade in current section title
+	$('.home-title').fadeIn();
+	window.setTimeout(function() {
+		console.log("hidden-title: animated fadeInLeft");
+		$('.hidden-title').addClass("animated fadeInLeft");
+		$('.hidden-title').show();
+	}, 1500);
+
+	// information per officer
+
 	$('.info-officer-group').hide();
 	$('.info-officer-photo').hover(function() {
 		var name = $(this).children().attr('data-trigger');
 		console.log(name + " is hovered.");
 		$('.'+name).show();
-		$('.'+name).removeClass("animated fadeOut");
-		$('.'+name).addClass("animated fadeIn");
+		$('.'+name).removeClass("animated fadeOutRight");
+		$('.'+name).addClass("animated fadeInLeft");
 		$(this).removeClass("photo-bw");
 	}, function() {
 		// leave hover
 		var name = $(this).children().attr('data-trigger');
 		console.log(name + " is hovered.");
-		$('.'+name).removeClass("animated fadeIn");
-		$('.'+name).addClass("animated fadeOut");
+		$('.'+name).removeClass("animated fadeInLeft");
+		$('.'+name).addClass("animated fadeOutRight");
 		$(this).addClass("photo-bw");
 	});
 });
